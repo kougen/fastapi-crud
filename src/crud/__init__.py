401f50efa5860b9262b4805f862cbe17be3be43a
Joshua Hegedus <josh.hegedus@outlook.com>
2024-04-01 21:11:07 +0200

    new exports

--- src/crud/__init__.py
+++ src/crud/__init__.py
@@ -1,5 +1,5 @@
 from .app import CRUDApi, CRUDApiRouter
-from .lib import Model, AuthConfig, UserEntity
+from .lib import Model, AuthConfig, UserEntity, USER_FIELDS
 from .entities import EntityFactory
 
-__all__ = ['CRUDApi', 'CRUDApiRouter', 'Model', 'EntityFactory', 'AuthConfig', 'UserEntity']
+__all__ = ['CRUDApi', 'CRUDApiRouter', 'Model', 'EntityFactory', 'AuthConfig', 'UserEntity', 'USER_FIELDS']
