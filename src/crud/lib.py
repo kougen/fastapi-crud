e5cb906aa0872ad24d025d68c38e047f34d907df
Joshua Hegedus <josh.hegedus@outlook.com>
2024-04-07 01:00:43 +0200

    added back incorrect rebase parts

--- src/crud/lib.py
+++ src/crud/lib.py
@@ -1,6 +1,5 @@
-from click.core import F
 from pydantic import BaseModel
-from pyrepositories import IdTypes, DataTable, Entity, FieldBase, FieldTypes, FieldKeyTypes
+from pyrepositories import DataTable, Entity, FieldBase, FieldTypes, FieldKeyTypes, Filter, FilterCombination, FilterCondition, FilterTypes, EntityField
 from fastapi.security import OAuth2PasswordBearer
 
 
@@ -26,3 +25,90 @@ def convert_dict_to_filter(data: dict) -> Filter:
         conditions.append(FilterCondition(key, value, FilterTypes.CONTAINS))
 
     return Filter(conditions, FilterCombination.AND)
+
+
+class User(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
+
+
+def decode_token(token: str):
+    return User(
+        username=token + "fakedecoded", email="john@doe.com", full_name="John Doe"
+    )
+
+
+class AuthConfig:
+    def __init__(self, users_db: DataTable, oauth2_scheme: OAuth2PasswordBearer):
+        self.users_db = users_db
+        self.oauth2_scheme = oauth2_scheme
+
+
+class UserEntity(Entity):
+    def __init__(self, username: str, email: str, full_name: str):
+        base = [
+            FieldBase("username", FieldTypes.STR, FieldKeyTypes.UNIQUE, username),
+            FieldBase("email", FieldTypes.STR, FieldKeyTypes.UNIQUE, email),
+            FieldBase("full_name", FieldTypes.STR, FieldKeyTypes.STANDARD, full_name),
+            FieldBase("disabled", FieldTypes.BOOL, FieldKeyTypes.STANDARD, False),
+            FieldBase("hashed_password", FieldTypes.STR, FieldKeyTypes.STANDARD, ""),
+        ]
+        entity_fields = []
+        for field in base:
+            entity_fields.append(EntityField(field))
+        super().__init__(entity_fields)
+        self.username = username
+        self.email = email
+        self.full_name = full_name
+        self.disabled = False
+
+    @property
+    def username(self):
+        return self.get_field("username")
+
+    @username.setter
+    def username(self, value):
+        self.set_field_value("username", value)
+
+    @property
+    def email(self):
+        return self.get_field("email")
+
+    @email.setter
+    def email(self, value):
+        self.set_field_value("email", value)
+
+    @property
+    def full_name(self):
+        return self.get_field("full_name")
+
+    @full_name.setter
+    def full_name(self, value):
+        self.set_field_value("full_name", value)
+
+    @property
+    def disabled(self):
+        return self.get_field("disabled")
+
+    @disabled.setter
+    def disabled(self, value):
+        self.set_field_value("disabled", value)
+
+    @property
+    def hashed_password(self):
+        return self.get_field("hashed_password")
+
+    @hashed_password.setter
+    def hashed_password(self, value):
+        self.set_field_value("hashed_password", value)
+
+
+USER_FIELDS = [
+    FieldBase("username", FieldTypes.STR, FieldKeyTypes.UNIQUE, ""),
+    FieldBase("email", FieldTypes.STR, FieldKeyTypes.UNIQUE, ""),
+    FieldBase("full_name", FieldTypes.STR, FieldKeyTypes.STANDARD, ""),
+    FieldBase("disabled", FieldTypes.BOOL, FieldKeyTypes.STANDARD, False),
+    FieldBase("hashed_password", FieldTypes.STR, FieldKeyTypes.STANDARD, ""),
+]
